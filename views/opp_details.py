47071c14f973e4f342255a30ed91581e4670d15d
Ege Güvener <93076687+egegvner@users.noreply.github.com>
2025-07-05 00:05:28 +0800

    Update opp_details.py

--- views/opp_details.py
+++ views/opp_details.py
@@ -1,12 +1,23 @@
 import streamlit as st
 import pydeck as pdk
 from datetime import datetime
-from utils import navigate_to, get_distance_km
+from utils import navigate_to, get_distance_km, reverse_geocode_location
 import pandas as pd
 import base64
 import time
 
 def opp_details(conn):
+    CATEGORY_COLORS = {
+    "Environment": "#09AD11",
+    "Education": "#3AA6FF",
+    "Health": "#B50000",
+    "Animal Welfare": "#FFAA00",
+    "Community Service": "#E456FD",
+    "Sports": "#00FFE5",
+    "Arts & Culture": "#440FCA",
+    "Disaster Relief": "#5C5C5C",
+    "Other": "#000000"
+}
     st.markdown("""
     <style>
     .opp-header {
@@ -26,7 +37,7 @@ def opp_details(conn):
         font-size: 3em;
     }
     .section-title {
-        font-size: 1.6em;
+        font-size: 1.8em;
         margin: 24px 0 8px 0;
         color: #333;
         border-bottom: 2px solid #e0e0e0;
@@ -77,10 +88,11 @@ def opp_details(conn):
     """, unsafe_allow_html=True)
 
     c = conn.cursor()
-    user_rating = c.execute("""
-        SELECT rating FROM individuals
-        WHERE id = ?
-    """, (st.session_state.user_id,)).fetchone()[0]
+    user_rating = c.execute(
+        "SELECT AVG(rating) FROM user_ratings WHERE user_id = ?", (st.session_state.user_id,)
+    ).fetchone()[0]
+
+    user_rating = user_rating if user_rating else 0
 
     opp_id = st.session_state.temp_opp_id
     if not opp_id:
@@ -88,8 +100,8 @@ def opp_details(conn):
         return
     
     user_lat, user_lon = c.execute("""
-        SELECT latitude, longitude FROM individuals
-        WHERE id = ?
+        SELECT latitude, longitude FROM users
+        WHERE user_id = ?
     """, (st.session_state.user_id,)).fetchone()
 
     c.execute("""
@@ -107,20 +119,41 @@ def opp_details(conn):
     (title, description, requirements, location, event_date,
      lat, lon, min_requred_rating, org_id, org_name, org_email) = row
 
-    c.execute("""
+    avg_rating = c.execute("""
         SELECT AVG(r.rating)
         FROM ratings r
         WHERE r.org_id = ?
-    """, (org_id,))
-    avg_rating = c.fetchone()[0]
+    """, (org_id,)).fetchone()[0]
     avg_rating = f"{avg_rating:.2f}" if avg_rating else "No ratings yet"
-
     c1, c2 = st.columns(2, gap="large")
     with c1:
+        c.execute("SELECT category FROM opportunities WHERE id = ?", (opp_id,))
+        category_row = c.fetchone()
+        category = category_row[0] if category_row else "Other"
+        base_color = CATEGORY_COLORS.get(category, CATEGORY_COLORS["Other"])
+        def lighten(hex_color, factor=0.6):
+            hex_color = hex_color.lstrip("#")
+            rgb = [int(hex_color[i:i+2], 16) for i in (0, 2, 4)]
+            light_rgb = [int(c + (255 - c) * factor) for c in rgb]
+            return f"#{''.join(f'{v:02X}' for v in light_rgb)}"
+        gradient_start = lighten(base_color, 0.4)
+        gradient_end = base_color
+
+        with st.spinner(""):
+            try:
+                @st.cache_data(show_spinner=False)
+                def get_location(lat, lon):
+                    return reverse_geocode_location(lat, lon)
+                loc = get_location(lat, lon)
+            except:
+                loc = ''
         st.markdown(f"""
-        <div class="opp-header">
-            <h1>{title}</h1><br>
-            <p>📍 {location} &nbsp; | &nbsp;  {event_date} &nbsp; | &nbsp; 🚩 {get_distance_km(user_lat, user_lon, lat, lon) if user_lat or user_lon else None} km</p>
+        <div class="opp-header" style="background: linear-gradient(to right, {gradient_start}, {gradient_end});">
+            <h1 style="font-family:Inter;">{title}</h1>
+            <span style="font-size:1em; color:rgba(255,255,255,0.6);">{loc}</span><br><br>
+            <span style="font-size:1em; color:rgba(255,255,255,0.8);">
+            📍 {location} &nbsp; | &nbsp;  {event_date} &nbsp; | &nbsp; 📌 {round(get_distance_km(user_lat, user_lon, lat, lon), 1) if user_lat or user_lon else None} km &nbsp; | &nbsp; ⭐️ {avg_rating[:3]}
+            </span></p>
         </div>
         """, unsafe_allow_html=True)
 
@@ -130,62 +163,62 @@ def opp_details(conn):
         st.markdown(f"<div class='section-title'><b>Requirements</b>", unsafe_allow_html=True)
         st.write(requirements or "No specific requirements.")
         with st.container(border=True):
-            st.write(f"Minimum Required Self-Rating: {"⭐️ {min_requred_rating}" if min_requred_rating else "**None!**"}")
+            st.write(f"Minimum Required Self-Rating: {f"⭐️ {min_requred_rating}" if min_requred_rating else "**None!**"}")
 
         st.markdown(f"<div class='section-title'><b>Organizer</b></div>", unsafe_allow_html=True)
-        st.write(f"#### **{org_name}** ⭐️ {avg_rating}")
+        st.write(f"##### **{org_name}** ⭐️ {avg_rating}")
         st.write(f"✉️ {org_email}")
         col1, col2 = st.columns(2)
         with col1:
             if st.button("💬 Chat with Organizer", use_container_width=True):
-                c.execute("""
-                    SELECT id FROM chats
-                    WHERE student_id = ? AND opportunity_id = ? AND org_id = ?
-                """, (st.session_state.user_id, opp_id, org_id))
-                chat = c.fetchone()
-                if not chat:
+                with st.spinner(""):
                     c.execute("""
-                        INSERT INTO chats (student_id, org_id, opportunity_id)
-                        VALUES (?, ?, ?)
-                    """, (st.session_state.user_id, org_id, opp_id))
-                    conn.commit()
-                    chat_id = c.lastrowid
-                else:
-                    chat_id = chat[0]
-                st.session_state.active_chat = chat_id
+                        SELECT id FROM chats
+                        WHERE user_id = ? AND opportunity_id = ? AND org_id = ?
+                    """, (st.session_state.user_id, opp_id, org_id))
+                    chat = c.fetchone()
+                    if not chat:
+                        c.execute("""
+                            INSERT INTO chats (user_id, org_id, opportunity_id)
+                            VALUES (?, ?, ?)
+                        """, (st.session_state.user_id, org_id, opp_id))
+                        conn.commit()
+                        chat_id = c.lastrowid
+                    else:
+                        chat_id = chat[0]
+                    st.session_state.active_chat = chat_id
+                    time.sleep(1)
                 navigate_to("chat")
         with col2:
             c.execute("""
                 SELECT id, status FROM applications
-                WHERE student_id = ? AND opportunity_id = ?
+                WHERE user_id = ? AND opportunity_id = ?
             """, (st.session_state.user_id, opp_id))
             appl = c.fetchone()
             if appl:
                 if appl[0] and appl[1] == "accepted":
-                    st.success("✅ Already Applied")
+                    st.info("✅ Accepted")
                 elif appl[0] and appl[1] == "rejected":
                     st.error("❌ Application Rejected")
                 elif appl[0] and appl[1] == "pending":
                     st.warning("⏳ Application Pending")
             else:
-                if st.button("✋ Apply Now", use_container_width=True, type="primary", disabled=(user_rating is None or user_rating < min_requred_rating)):
+                if st.button("✋ Apply Now", use_container_width=True, type="primary", disabled=(user_rating < min_requred_rating), help="Your self-rating is too low to apply." if user_rating < min_requred_rating else None):
                     with st.spinner("Submitting application..."):
                         c.execute("""
                             INSERT INTO applications
-                            (student_id, opportunity_id, status, application_date)
+                            (user_id, opportunity_id, status, application_date)
                             VALUES (?, ?, 'pending', datetime('now'))
                         """, (st.session_state.user_id, opp_id))
                         conn.commit()
                         time.sleep(4)
-
-                    st.success("Application submitted!")
                     st.rerun()
 
-        st.markdown(f"<div class='section-title'>Student Reflections</div>", unsafe_allow_html=True)
+        st.markdown(f"<div class='section-title'><b>User Reflections ({c.execute("SELECT COUNT(reflection) FROM ratings WHERE opportunity_id = ?", (opp_id,)).fetchone()[0]})</b></div>", unsafe_allow_html=True)
         c.execute("""
             SELECT r.rating, r.reflection, r.created_at, u.name
             FROM ratings r
-            JOIN individuals u ON r.student_id = u.id
+            JOIN users u ON r.user_id = u.user_id
             WHERE r.opportunity_id = ?
             ORDER BY r.created_at DESC
             LIMIT 6
@@ -193,13 +226,13 @@ def opp_details(conn):
         reflections = c.fetchall()
         if reflections:
             html = '<div class="reflection-grid">'
-            for rating, text, created_at, student in reflections:
+            for rating, text, created_at, user in reflections:
                 stars = "★" * rating + "☆" * (5 - rating)
                 date_str = datetime.strptime(created_at, "%Y-%m-%d %H:%M:%S")\
                                 .strftime("%b %d, %Y")
                 html += f"""
                 <div class="reflection-card">
-                <h4>{student}</h4>
+                <h4>{user}</h4>
                 <div class="stars">{stars}</div>
                 <div class="text">{text}</div>
                 <div class="meta">{date_str}</div>
@@ -219,8 +252,9 @@ def opp_details(conn):
             "Color": [0, 255, 0],
             "Title": title,
             "Organizer": org_name,
-            "Rating": avg_rating,
+            "Rating": avg_rating[:3],
             "Location": location,
+            "Dist": round(get_distance_km(user_lat, user_lon, lat, lon), 1) if user_lat or user_lon else None
         }])
 
         st.pydeck_chart(pdk.Deck(
@@ -245,10 +279,11 @@ def opp_details(conn):
             tooltip={
             "html": """
                 <div style='font-family: Inter;'>
-                <span style='font-size: 1.5em; font-weight: bold;'><b>{Title}</b></span><br/><hr>
-                <span>📍 {Location}</span><br/>
-                <span>🏢 {Organizer}</span><br/>
-                <span>⭐ {Rating}</span>
+                <span style='font-size: 1.4em; font-weight: bold;'><b>{Title}</b></span><br/><hr>
+                <span>📍 {Location}</span><br>
+                <span>🏢 {Organizer}</span><br>
+                <span>⭐ {Rating}</span><br>
+                <span>📌 {Dist} km</span>
                 </div>
             """,
             "style": {
