65c9b7f14383a0d169748bd966492d693c951680
Ege Güvener <93076687+egegvner@users.noreply.github.com>
2025-07-08 14:46:34 +0800

    Update landing.py

--- views/landing.py
+++ views/landing.py
@@ -2,19 +2,8 @@ import streamlit as st
 from utils import navigate_to
 import time
 
-def landing_page():
-    st.markdown('''
-    <style>
-            [data-testid="stAppViewContainer"] {
-            background: linear-gradient(90deg, #ffffff, #cfe6ff)
-                }
-            [data-testid="stHeader"]{
-            background: linear-gradient(90deg, #ffffff, #cfe6ff)
-                }
-    </style>
-    ''', unsafe_allow_html=True)
-    
-    st.markdown("<h1 style='font-family: Inter;'>CommiUnity</h1>", unsafe_allow_html=True)
+def landing_page(): 
+    st.markdown("<h1 style='font-family: Inter;'>VolunTree</h1>", unsafe_allow_html=True)
     st.markdown("<p style='font-family: Inter;'>Unite. Engage. Transform.</p>", unsafe_allow_html=True)
     
     for i in range(7):
@@ -25,16 +14,12 @@ def landing_page():
         st.markdown("<h3 style='font-family: Inter;'>Own an Account?</h3>", unsafe_allow_html=True)
         st.text("")
         if st.button("Login", key="login_button", use_container_width=True, type="primary", icon=":material/arrow_forward:"):
-            with st.spinner(""):
-                time.sleep(1)
             navigate_to("login")
 
     with col2:
         st.markdown("<h3 style='font-family: Inter;'>New to the App?</h3>", unsafe_allow_html=True)
         st.text("")
         if st.button("Register", key="register_button", use_container_width=True, type="primary", icon=":material/arrow_forward:"):
-            with st.spinner(""):
-                time.sleep(1)
             navigate_to("register")
     
     for i in range(5):
@@ -43,7 +28,7 @@ def landing_page():
     
     st.markdown("<h3 style='font-family: Inter;'>About this app</h3>", unsafe_allow_html=True)
     st.markdown("""<p style='font-family: Inter;'>
-    CommiUnity connects students with volunteer opportunities 
+    VolunTree connects students with volunteer opportunities 
     and organizations seeking volunteers. Browse opportunities, apply to events, and track 
     your volunteering journey all in one place.
     </p>""", unsafe_allow_html=True)
