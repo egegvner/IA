83acd154aa87e835f22e7c6f71fef8fe58feb4ca
Ege Güvener <93076687+egegvner@users.noreply.github.com>
2025-07-15 18:16:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -41,13 +41,7 @@ def main():
             initial_sidebar_state="expanded"
         )
 
-    DB_PATH = "./voluntree.db"
-    WRITABLE_PATH = "/tmp/voluntree.db"
-
-    if not os.path.exists(WRITABLE_PATH):
-        shutil.copy(DB_PATH, WRITABLE_PATH)
-
-    conn = get_db_connection(WRITABLE_PATH=WRITABLE_PATH)
+    conn = get_db_connection("./voluntree.db")
 
     controller = CookieController()
     try:
